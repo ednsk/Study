70342240210d3ab40027fc96aedd8ce97aef411b
Eduard Gilmiyarov <ednsk@outlook.com>
2022-11-19 16:44:04 +0500

    v1.2 - Add styles and fix php

--- PHP + HTML.php
+++ PHP + HTML.php
@@ -1,9 +1,27 @@
-<div style ='border: 1px solid; padding: 30px;
-font-size: 25px; background: #c6c6c6; color: black; border-radius: 20px; text-align: center; margin-top: 30px;'>Hello World<div>
-<button style="padding:10px 50px; cursor: pointer;" name="hi" method="POST">И тебе привет!</button>
+<style>
+.button_hi {
+    padding:10px 50px; 
+    cursor: pointer;
+}
+.Hello_World_Block {
+    border: 1px solid; 
+    padding: 30px;
+    font-size: 25px; 
+    background: #c6c6c6; 
+    color: black; 
+    border-radius: 20px; 
+    text-align: center; 
+    margin-top: 30px;
+}
+</style>
+
+<div class ="Hello_World_Block">
+<p>Hello World</p>
+<button class="button_hi" name="hi" method="POST">И тебе привет!</button>
+</div>
 
 <?php
-if (isset($_POST)) {
+if (isset($_POST["hi"])) {
     echo 'Красавчег!';
 }
 ?>
